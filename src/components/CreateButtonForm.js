abffcdd5b224e18a075609b81e9a397869175b52
Brucegx <guoxi.bruce@gmail.com>
2018-01-22 23:33:43 -0500

    second field

--- src/components/CreateButtonForm.js
+++ src/components/CreateButtonForm.js
@@ -1,28 +1,61 @@
-import React from 'react';
-import { Form, Input} from 'antd';
+import React from "react";
+import { Form, Input, Upload, Icon } from "antd";
 const FormItem = Form.Item;
 
 class createButtonForm extends React.Component {
+  normFile = (e) => {
+    console.log('Upload event:', e);
+    if (Array.isArray(e)) {
+      return e;
+    }
+    return e && e.fileList;
+  }
   render() {
     const { getFieldDecorator } = this.props.form;
     const formItemLayout = {
       labelCol: { span: 6 },
-      wrapperCol: { span: 14 },
+      wrapperCol: { span: 14 }
     };
     return (
       <Form layout="vertical">
-          <FormItem
-            {...formItemLayout}
-            label="Message"
-          >
-            {getFieldDecorator('message', {
-              rules: [{ required: true, message: 'Please input the title of collection!' }],
+        <FormItem {...formItemLayout} label="Message">
+          {getFieldDecorator("message", {
+            rules: [
+              {
+                required: true,
+                message: "Please input the title of collection!"
+              }
+            ]
+          })(<Input />)}
+        </FormItem>
+        <FormItem {...formItemLayout} label="Image">
+          <div className="dropbox">
+            {getFieldDecorator("image", {
+              valuePropName: "fileList",
+              getValueFromEvent: this.normFile,
+               rules: [
+              {
+                required: true,
+                message: "Please select an image!"
+              }
+            ]
             })(
-              <Input />
+              <Upload.Dragger name="files" action="/upload.do">
+                <p className="ant-upload-drag-icon">
+                  <Icon type="inbox" />
+                </p>
+                <p className="ant-upload-text">
+                  Click or drag file to this area to upload
+                </p>
+                <p className="ant-upload-hint">
+                  Support for a single or bulk upload.
+                </p>
+              </Upload.Dragger>
             )}
-          </FormItem>
-        </Form>
-        );
-     }    
+          </div>
+        </FormItem>
+      </Form>
+    );
+  }
 }
 export const WrappedCreatePostForm = Form.create()(createButtonForm);
